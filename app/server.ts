0ef739c41ad3c41a025f1e7fb8697adeac70db6d
Mario Uhrin <mario.uhrin@gmail.com>
2019-03-10 21:18:40 +0100

    add morgan logger and update error handling

--- app/server.ts
+++ app/server.ts
@@ -1,19 +1,31 @@
-import express from 'express';
-import errorHandler from 'errorhandler';
+import express, { Response, NextFunction } from 'express';
+import morgan from 'morgan';
 import { MongoClient } from 'mongodb';
 
 import { Routes } from './routes';
 import { config } from './config';
 import { IExtendedExpressApp } from './types/interfaces/app-express.interface';
+import { IExtendedRequest } from './types/interfaces/request.interface';
 import logger from './config/logger';
 
 const app: IExtendedExpressApp = express();
 const port: number = Number(process.env.PORT) || 3000;
 
+// add morgan logger to express
+app.use(morgan('dev'));
+
+// add all routes to express
 app.use('/api', Routes);
 
-app.use(errorHandler());
+// global error handling
+app.use((err: Error, req: IExtendedRequest, res: Response, next: NextFunction) => {
+  if (err.message.includes('Not found')) {
+    return res.status(404).send(err.message);
+  }
+  res.send('Something goes wrong');
+});
 
+// run mongodb and express server
 async function initServerAndDB() {
   try {
     const { mongoDbName, mongoUrl } = config;
