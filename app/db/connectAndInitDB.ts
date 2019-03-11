c41943c7889973122ef5bfaac0c5b807a8d0758f
Mario Uhrin <mario.uhrin@gmail.com>
2019-03-11 01:02:51 +0100

    simple refactoring

--- app/db/connectAndInitDB.ts
+++ app/db/connectAndInitDB.ts
@@ -1,30 +1,28 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
-import { createCustomersCollection, createSequenceCollection } from "./createCollections";
-import { transformData } from "./transformData";
-import { config } from "../config";
+import { createCustomersCollection, createSequenceCollection } from './createCollections';
+import { transformData } from './transformData';
+import { config } from '../config';
 
 async function connectAndInitDB() {
   const { mongoUrl, mongoDbName } = config;
 
   try {
-    const connection: MongoClient = await MongoClient.connect(mongoUrl!, {
+    const client: MongoClient = await MongoClient.connect(mongoUrl!, {
       useNewUrlParser: true
     });
 
-    const db = connection.db(mongoDbName);
+    const db = client.db(mongoDbName);
 
     const customersCollection = await createCustomersCollection(db);
     const initialCustomersData = transformData();
     await customersCollection.createIndex({ index: 1 }, { unique: true });
     await customersCollection.insertMany(initialCustomersData);
-    console.log("inserted initial customers data to db");
+    console.log('inserted initial customers data to db');
 
     const sequenceCollection = await createSequenceCollection(db);
-    await sequenceCollection.insertOne({ seqNumber: 33, seqRef: "ref" });
+    await sequenceCollection.insertOne({ seqNumber: 33, seqRef: 'ref' });
     process.exit();
-
-    return db;
   } catch (err) {
     console.error(`Unable to connect to db with error: ${err}`);
   }
--- app/server.ts
+++ app/server.ts
@@ -9,7 +9,7 @@ import { IExtendedRequest } from './types/interfaces/request.interface';
 import logger from './config/logger';
 
 const app: IExtendedExpressApp = express();
-const port: number = Number(process.env.PORT) || 3000;
+const port: number = Number(process.env.PORT);
 
 // add morgan logger to express
 app.use(morgan('dev'));
@@ -29,20 +29,22 @@ app.use((err: Error, req: IExtendedRequest, res: Response, next: NextFunction) =
 async function initServerAndDB() {
   try {
     const { mongoDbName, mongoUrl } = config;
-    const connection: MongoClient = await MongoClient.connect(mongoUrl, {
+    const client: MongoClient = await MongoClient.connect(mongoUrl, {
       useNewUrlParser: true
     });
-    const db = connection.db(mongoDbName);
+    const db = client.db(mongoDbName);
     logger.info(`Connected to Mongodb: ${mongoDbName}`);
 
     app.locals.db = db;
-  } catch (err) {
-    logger.error(`Cannot connect to mongodb in hapi mongodb-plugin: ${err}`);
-  }
+    app.locals.dbClient = client;
 
-  app.listen(port, () => {
-    logger.info(`Listening at http://localhost:${port}`);
-  });
+    app.listen(port, () => {
+      logger.info(`Listening at http://localhost:${port}`);
+    });
+  } catch (err) {
+    logger.error(`Error in mongodb connection or server listening: ${err}`);
+  }
 }
 
+// initialize app
 initServerAndDB();
